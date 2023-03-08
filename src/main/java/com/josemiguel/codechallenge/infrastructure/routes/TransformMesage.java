6404b85122928edb76b886f77b87d173143e8942
jose miguel <josemiguel.besada@finleap.com>
2023-03-08 13:57:12 +0100

    pipeline

--- src/main/java/com/josemiguel/codechallenge/infrastructure/routes/TransformMesage.java
+++ src/main/java/com/josemiguel/codechallenge/infrastructure/routes/TransformMesage.java
@@ -3,14 +3,16 @@ package com.josemiguel.codechallenge.infrastructure.routes;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import com.josemiguel.codechallenge.infrastructure.routes.dto.CodeChallengeMessage;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Component
 @Slf4j
 public class TransformMesage {
 
-	public String truncate(String message) {
-		log.info("Filtering message:" + message);
-		return StringUtils.substring(message, 0, 40);
+	public CodeChallengeMessage truncate(String message) {
+		return CodeChallengeMessage.builder().rawMessage(StringUtils.abbreviate(message, 50)).build();
+		
 	}
 }
