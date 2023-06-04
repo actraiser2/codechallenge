cca4cfd6884c160c8259e79be904e1dd8455510c
jose miguel <josemiguel.besada@finleap.com>
2023-06-04 22:54:48 +0200

    HelthIndicator

--- src/main/java/com/josemiguel/codechallenge/infrastructure/healthchecks/CodeChallengeHealthIndicator.java
+++ src/main/java/com/josemiguel/codechallenge/infrastructure/healthchecks/CodeChallengeHealthIndicator.java
@@ -14,7 +14,7 @@ public class CodeChallengeHealthIndicator implements HealthIndicator {
 
 	@Override
 	public Health health() {
-		log.info("Calling codeChallengeHealthIndicator23" + 
+		log.info("Calling codeChallengeHealthIndicator3" + 
 				Paths.get("/tmp/codechallenge_readiness").toFile().exists());
 		if (!Paths.get("/tmp/codechallenge_readiness").toFile().exists()) {
 			return Health.up().build();
